6b1cda68a4169f78c9147ca237fa40ce0800e0a1
Christian Boin <christianboin@Christians-MacBook-Pro-2.local>
2021-03-06 14:41:31 -0500

    done for the day lol:

--- workers/trainer.py
+++ workers/trainer.py
@@ -13,14 +13,8 @@ def run(experiment_dir, root_dir):
     log.info("Trainer launched successfully.")
     
     conf = config.Default()
-    processed_df = util.initialize_dataframe(os.path.join(root_dir, conf.rel_path_to_raw_data), 
+    processed_df = util.collect_processed_data(os.path.join(root_dir, conf.rel_path_to_raw_data), 
                                             os.path.join(root_dir, conf.rel_path_to_proc_data),
                                             index_col='rowid',
                                             drop_cols=conf.raw_data_drop_cols)
     print(processed_df.head())
-<<<<<<< HEAD
-    
-=======
-    
-    
->>>>>>> 9d61a3cc1be85527995eab311d6495b6cc81d9a7
